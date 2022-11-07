b4f6c6c5ede34d4579c40e47193b5a1e0cc559f4
horatiu.dan <horatiu.dan@tangoe.com>
2022-11-07 17:46:23 +0200

    Adds Minifig class.

---
+++ src/app/minifig.spec.ts
@@ -0,0 +1,7 @@
+import { Minifig } from './minifig';
+
+describe('Minifig', () => {
+  it('should create an instance', () => {
+    expect(new Minifig()).toBeTruthy();
+  });
+});
---
+++ src/app/minifig.ts
@@ -0,0 +1,5 @@
+export class Minifig {
+  id: number;
+  name: string;
+  code: string;
+}
